a0989e6b69d924a1754c80af022672361a2fef22
VinhNguyen <vinh.nguyenthe@di4l.vn>
2021-06-28 13:12:19 +0700

    test auto build coverage

--- test/category_easy_test.dart
+++ test/category_easy_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:category_easy/category_easy.dart';
-
 void main() {
   testWidgets('description', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
