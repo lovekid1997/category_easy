c597f5b6ab4ad992e327f881b64eee59bf1c8aa7
VinhNguyen <vinh.nguyenthe@di4l.vn>
2021-06-28 20:06:19 +0700

    test function

--- test/category_easy_test.dart
+++ test/category_easy_test.dart
@@ -1,7 +1,12 @@
+import 'package:category_easy/category_easy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  test('Empty Email Test', () {
+    var test = caculator(1);
+    expect(test, 2);
+  });
   testWidgets('description', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
