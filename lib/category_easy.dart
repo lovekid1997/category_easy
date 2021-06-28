9dd67a471e0d93674105e8f0f08ae5b7f0e3e0b8
VinhNguyen <vinh.nguyenthe@di4l.vn>
2021-06-28 12:24:51 +0700

    no message

--- lib/category_easy.dart
+++ lib/category_easy.dart
@@ -1,9 +1,3 @@
 library category_easy;
 
 export 'category_easy_widget.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
--- test/category_easy_test.dart
+++ test/category_easy_test.dart
@@ -4,12 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:category_easy/category_easy.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
   testWidgets('description', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
